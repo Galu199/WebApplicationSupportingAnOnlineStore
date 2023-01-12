ca47e8ea80cee2b2565b7e64bc04b8662a532d22
Galu199 <Seweryngalu199@gmail.com>
2023-01-12 16:32:12 +0100

    PublishedApp v.1.0.1
    To start app run:
    WebApplicationSupportingAnOnlineStore.exe
    
    This app require Microsoft Sql Server.
    To migrate data-base use command:
    update-database
    in NuGet console.
    Or use DataBase.sql script to create database and fill it with example values with Microsoft Sql Sever Managment Studio.
    
    Lot's of Bugs!

---
+++ wwwroot/js/map.js
@@ -0,0 +1,41 @@
+﻿var map = L.map('map').setView([50.28863, 18.67745], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([50.28863, 18.67745]).addTo(map);
+
+var circle = L.circle([50.28863, 18.67745], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map);
+
+/*var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);*/
+
+marker.bindPopup("<b>Hello Guest!</b><br>I am a POLSL Wydział Automatyki Elektroniki i Informatyki.").openPopup();
+//circle.bindPopup("");
+//polygon.bindPopup("I am a polygon.");
+
+/*var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);*/
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $("img[src!='']").one("load", function () {
+        $(this).removeClass("placeholder");
+    }).each(function () {
+        if (this.complete) {
+            $(this).trigger('load');
+        }
+    });
+});
